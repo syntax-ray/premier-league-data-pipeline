166056209e4262ffdeeb107f0af3eb8c0b576a7d
Donald Ray <syntaxray@gmail.com>
2026-05-27 12:33:18 +0100

    Update entry behaviour

--- scripts/fetch_matches.py
+++ scripts/fetch_matches.py
@@ -7,8 +7,8 @@ import pandas as pd
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 CONN = http.client.HTTPSConnection("v3.football.api-sports.io")
-LEAGUES_SAVE_PATH = os.path.join(os.getcwd(), 'data', 'leagues.json')
-LEAGUES_SUMMARY_SAVE_PATH = os.path.join(os.getcwd(), 'data', 'leagues_summary.csv')
+AVALABLE_LEAGUES_SAVE_PATH = os.path.join(os.getcwd(), 'data', 'leagues.json')
+AVAILABLE_LEAGUES_SUMMARY_SAVE_PATH = os.path.join(os.getcwd(), 'data', 'leagues_summary.csv')
 
 HEADERS = {
     'x-apisports-key': API_KEY
@@ -25,22 +25,22 @@ def get_api_status():
     except Exception as e:
         print(f'Could not get status due to {e}')
 
-def fetch_leagues():
+def fetch_available_leagues():
     try:
         CONN.request("GET", "/leagues", headers=HEADERS)
         print('Successfully fetched leagues')
         response = CONN.getresponse().read().decode()
         leagues = json.loads(response)['response']
-        with open(LEAGUES_SAVE_PATH, 'w') as f:
+        with open(AVALABLE_LEAGUES_SAVE_PATH, 'w') as f:
             json.dump(leagues, f)
         print('Saved leagues data to file')
         get_api_status()
     except Exception as e:
         print(f'Could not fetch leagues due to {e}')
     
-def leagues_to_summary_csv():
+def available_leagues_to_summary_csv():
     leagues = None
-    with open(LEAGUES_SAVE_PATH, 'r') as f:
+    with open(AVALABLE_LEAGUES_SAVE_PATH, 'r') as f:
         leagues = json.load(f)
     summary = {
         'id': [],
@@ -57,11 +57,14 @@ def leagues_to_summary_csv():
         summary['country'].append(league['country']['name'])
 
     summary_df = pd.DataFrame(summary)
-    summary_df.to_csv(path_or_buf=LEAGUES_SUMMARY_SAVE_PATH, index=False)
+    summary_df.to_csv(path_or_buf=AVAILABLE_LEAGUES_SUMMARY_SAVE_PATH, index=False)
 
 
 if __name__ == '__main__':
     get_api_status()
-    fetch_leagues()
-    leagues_to_summary_csv()
+    if not os.path.exists(AVALABLE_LEAGUES_SAVE_PATH):
+        fetch_available_leagues ()
+        available_leagues_to_summary_csv()
+    if not os.path.exists(AVAILABLE_LEAGUES_SUMMARY_SAVE_PATH):
+        available_leagues_to_summary_csv()
     
